1ca95649f21c366bc3f72fbfd1b8a08859eda258
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-05-15 13:58:09 +0200

    zmiany w pakiecie - nowe mapy funkcja fast_ggmap czyszczenie

--- R/fast_ggmap.R
+++ R/fast_ggmap.R
@@ -4,38 +4,62 @@ fast_ggmap <- function(data,
                        id,
                        var,
                        maptype = 'powiat',
-                       higher_level = F) {
-  require(MapOfPoland)
-  
-  
-  if (maptype == 'powiat') {
-    data(LAU1, package = 'MapOfPoland')
-    
-    data_map <- fortify(LAU1) %>%
-      mutate(id = as.numeric(id)) %>%
-      left_join(LAU1 %>%
-                  as.data.frame() %>%
-                  select(id, code) %>%
-                  mutate(id = as.numeric(id))) %>%
-      tbl_df() %>%
-      mutate(region = as.character(code))
+                       higher_level = T) {
+
+
+  if (maptype == 'gmi') {
+    data(lau2, package = 'MapOfPoland')
+    data_map <- fortify(lau2, region="jpt_kod_je")
   }
-  
-  
-  p <- ggplot() +
-    geom_path(data = data_map,
-              aes(x = long,
-                  y = lat,
-                  group = region)) +
-    geom_map(
-      data = data,
-      aes_string(map_id = id,
-                 fill = var),
-      map = data_map,
-      col = 'black'
+
+  if (maptype == 'powiat') {
+    data(lau1, package = 'MapOfPoland')
+    data_map <- fortify(lau1, region="jpt_kod_je")
+  }
+
+  if (maptype == 'woj') {
+    data(nts2, package = 'MapOfPoland')
+    data_map <- fortify(nts2, region="jpt_kod_je")
+  }
+
+  data_map <- left_join(x = data_map,
+                        y = data,
+                        by = c( 'group' = id))
+
+  ## normal map
+
+  p1 <- ggplot() +
+    geom_polygon(
+      data = data_map,
+      aes_string(x = long,
+                 y = lat,
+                 group = group),
+      color = "black",
+      size = 0.25,
+      fill = var
     ) +
-    scale_fill_viridis() +
-    ggmap::theme_nothing(legend = TRUE)
-  
-  return(p)
+    theme_nothing(legend = TRUE) +
+    coord_map()
+
+  ## add higher level
+  if (higher_level) {
+    if (maptype != 'woj') {
+      data(nts2, package = 'MapOfPoland')
+      woj <- fortify(nts2, region="jpt_kod_je")
+
+      p1 <- p1 +
+        geom_polygon(
+          data = woj,
+          aes_string(x = long,
+                     y = lat,
+                     group = group),
+          color = "black",
+          size = 1,
+          fill = NA
+        ) +
+        theme_nothing(legend = TRUE)
+    }
+  }
+
+  return(p1)
 }
--- R/spdf_to_df.R
+++ R/spdf_to_df.R
@@ -1,5 +1,5 @@
 #' SpatialPolygonsDataFrame to data.frame function
-#' 
+#'
 #' This function convert SpatialPolygonsDataFrame to data.frame witout droping any variable
 #'
 #' @param spdf - SpatialPolygonsDataFrame
@@ -9,13 +9,16 @@
 #' @keywords sp, ggplot2
 #'
 #' @export
-#' 
+#'
 #' @examples
 #' # data("wojewdztwa")
 #' # spdf_to_df(wojewodztwa)
 
-spdf_to_df <- function(spdf){
-        df <- fortify(spdf)
-        join(df, spdf@data, by="id")
+spdf_to_df <- function(spdf) {
+  df <- fortify(spdf) %>%
+    mutate(id = as.numeric(id)) %>%
+    left_join(spdf@data)
 }
+
+
 
