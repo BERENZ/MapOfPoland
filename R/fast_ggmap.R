61081c8eee9ff9ec16effbe23aaf458877657c5f
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-05-15 22:14:09 +0200

    mapy podregionow mapy regionow uwzglednienie zmian funkcji fast_ggmap

--- R/fast_ggmap.R
+++ R/fast_ggmap.R
@@ -25,18 +25,27 @@ fast_ggmap <- function(data,
                        maptype = 'powiat',
                        higher_level = T) {
   if (maptype == 'gmi') {
-    data(lau2, package = 'MapOfPoland')
-    data_map <- fortify(lau2, region = "jpt_kod_je")
+    data_map <- fortify(MapOfPoland::lau2, region = "jpt_kod_je")
   }
 
   if (maptype == 'powiat') {
-    data(lau1, package = 'MapOfPoland')
-    data_map <- fortify(lau1, region = "jpt_kod_je")
+    data_map <- fortify(MapOfPoland::lau1, region = "jpt_kod_je")
+  }
+
+  if (maptype == 'podreg_66') {
+    data_map <- fortify(MapOfPoland::nts3_66, region = "jpt_kod_je")
+  }
+
+  if (maptype == 'podreg_72') {
+    data_map <- fortify(MapOfPoland::nts3_72, region = "jpt_kod_je")
   }
 
   if (maptype == 'woj') {
-    data(nts2, package = 'MapOfPoland')
-    data_map <- fortify(nts2, region = "jpt_kod_je")
+    data_map <- fortify(MapOfPoland::nts2, region = "jpt_kod_je")
+  }
+
+  if (maptype == 'region') {
+    data_map <- fortify(MapOfPoland::nts1, region = "jpt_kod_je")
   }
 
   data_map <- left_join(x = data_map,
@@ -63,8 +72,7 @@ fast_ggmap <- function(data,
   ## add higher level
   if (higher_level) {
     if (maptype != 'woj') {
-      data(nts2, package = 'MapOfPoland')
-      woj <- fortify(nts2, region = "jpt_kod_je")
+      woj <- fortify(MapOfPoland::nts2, region = "jpt_kod_je")
 
       p1 <- p1 +
         geom_polygon(
